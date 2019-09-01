7ccdc13af7b663caf6c9408be9752d2a4890e3dd
gabrielbaez04 <gabrielbaez04@gmail.com>
2019-09-01 16:53:53 -0400

    Updates

---
+++ precache-manifest.056f0fe55f4e4f1be9b9b72e55e62855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7feb8eeb548bf4fad37dc4f5113996",
+    "url": "/movie-theater/index.html"
+  },
+  {
+    "revision": "cf66323738c1a41c419f",
+    "url": "/movie-theater/static/css/main.1ac9862c.chunk.css"
+  },
+  {
+    "revision": "bf19be023a9b631b26e1",
+    "url": "/movie-theater/static/js/2.45403cbd.chunk.js"
+  },
+  {
+    "revision": "cf66323738c1a41c419f",
+    "url": "/movie-theater/static/js/main.07d16983.chunk.js"
+  },
+  {
+    "revision": "e08cddc5d1395dc8352e",
+    "url": "/movie-theater/static/js/runtime~main.d4492ca4.js"
+  }
+]);
